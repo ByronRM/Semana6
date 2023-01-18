b516f30bcd970ef1d5da81d063f5b3ae12b68d57
byronromeromunoz <bromero@cyliconlab.com>
2023-01-18 04:25:15 -0500

    first commit

---
+++ Semana5/Semana5/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Semana5
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.100.130:8888/moviles/post.php";
+        private readonly HttpClient cliente = new HttpClient();
+        private ObservableCollection<Semana5.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnGet_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var content = await cliente.GetStringAsync(Url);
+            List<Semana5.Datos> posts = JsonConvert.DeserializeObject<List<Semana5.Datos>>(content);
+            _post = new ObservableCollection<Semana5.Datos>(posts);
+
+            MyListView.ItemsSource = _post;
+
+        }
+
+
+      void btnInsertar_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+       
+    }
+}
+
---
+++ Semana5/Semana5/Registro.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Xamarin.Forms;
+
+namespace Semana5
+{
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+       void btnInsertar_Clicked(Object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtId.Text);
+                parametros.Add("nombre ", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://192.168.100.130:8888/moviles/post.php", "POST", parametros);
+               
+              DisplayAlert("Alerta", "Dato ingresado conrrectamente", "Ok");
+                Navigation.PushAsync(new MainPage());
+
+            }
+            catch (Exception ex)
+            {
+                 DisplayAlert("Mensaje de error", "Error" + ex.Message,"Ok");
+            }
+            
+        }
+
+        void btnCancelar_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
+
